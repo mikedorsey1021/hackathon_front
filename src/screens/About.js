4ae1c8e0399f1910e0d1cdd79262162ea66db557
Wes <wesleySalesberry@hotmail.com>
2020-10-17 23:47:39 -0400

    started the about page

---
+++ src/screens/About.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+import style from 'styled-components';
+import CheckboxForm  from '../components/CheckboxForm/CheckboxForm';
+
+
+export const About = () => {
+
+
+    return (
+        <Fragment>
+        </Fragment>
+    )
+}
+
